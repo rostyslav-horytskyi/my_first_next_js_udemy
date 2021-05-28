c12a233787108b7171940cf5f9ce6004d6d9be5d
gorzer <gorzer.rostyslav@gmail.com>
2021-05-28 21:45:27 +0300

    feat: small project from udemy

---
+++ components/layout/layout.js
@@ -0,0 +1,25 @@
+import { Fragment, useContext } from 'react';
+import NotificationContext from '../../store/notification-context';
+import Notification from '../ui/notification';
+
+import MainHeader from './main-header';
+
+function Layout(props) {
+  const { notification } = useContext(NotificationContext);
+
+  return (
+    <Fragment>
+      <MainHeader />
+      <main>{props.children}</main>
+      {notification && (
+        <Notification
+          title={notification.title}
+          message={notification.message}
+          status={notification.status}
+        />
+      )}
+    </Fragment>
+  );
+}
+
+export default Layout;
